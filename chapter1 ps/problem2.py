5971d2586918708e490a37e6a987409cbb86ac21
Jashan Mehra <jashanmehra007@gmail.com>
2026-03-27 22:01:41 +0530

    okk

--- chapter1 ps/problem2.py
+++ chapter1 ps/problem2.py
@@ -1,3 +1,6 @@
 import pyttsx3
-pyttsx3.speak("my name is jashan")
-
+print("pyttsx3 imported sucessfully")
+engine = pyttsx3.init()
+engine.say("Hello Jashan, How are you")
+engine.runAndWait()
+print("Speech Jashan")
