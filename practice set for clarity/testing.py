de03b0cad250d3642a000acd15e05f1b00035047
Jashan Mehra <jashanmehra007@gmail.com>
2026-03-29 12:59:48 +0530

    OK

---
+++ practice set for clarity/testing.py
@@ -0,0 +1,47 @@
+x = 10
+y = 20
+result = x > y
+print(result)
+print(type(result))
+
+num = 4
+is_even = num%2 ==0
+print(is_even)
+
+num = int(input("Enter a number: "))
+is_even = num % 2 == 0
+
+if is_even:
+    print("Even")
+else:
+    print("Odd") 
+
+x = "100"
+y = int(x)
+z = y + 50
+print(z)
+print(type(z))
+
+a = "3.14"
+b = int(float(a))
+print(b)
+
+value = 0
+print(bool(value))
+value = 10
+print(bool(value))
+
+s = "2026"  
+c = int(s) +10
+result = str(c)
+print(result)
+print(type(result))
+
+x,y,z =100, 200, 300
+a = x+y+z
+print(a)
+
+var = 42
+print(type(var))
+var = "42"
+print(type(var))
---
+++ practice set for clarity/tseting1.py
@@ -0,0 +1,4 @@
+height, weight, age = 160.02, 43, 22
+BMI = weight/(height/100)**2
+print(BMI)
+
