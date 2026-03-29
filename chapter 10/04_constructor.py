a3ce24c56c8a68e51ece683eb157eb94d925dccc
Jashan Mehra <jashanmehra007@gmail.com>
2026-03-29 16:34:35 +0530

    ok

--- chapter 10/04_constructor.py
+++ chapter 10/04_constructor.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Employee:
     language = "Python"
     salary = 120000
@@ -22,30 +21,3 @@ harry = Employee("Harry",130000,"JavaScript")
 print(harry.name, harry.salary, harry.language)
 
 rohan = Employee()
-
-=======
-class Employee:
-    language = "Python"
-    salary = 120000
-
-    def __init__(self,name,salary,language):
-        self.name = name
-        self.salary = salary
-        self.language = language
-        print("i am creating an object")
-        
-    def getinfo(self):
-        print(F"The language is{self.language}. The salary is {self.salary}")
-    
-    @static method
-
-    def greet():
-        print("Good morning")
-
-harry = Employee("Harry",130000,"JavaScript")
-#harry.name = "Harry"
-print(harry.name, harry.salary, harry.language)
-
-rohan = Employee()
-
->>>>>>> 06e56d1c0f03ad01d4fd6bb656a244e13c3084c1
