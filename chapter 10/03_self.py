2ad7fd15f6dd782edfcd76286d774bf91a477bec
Jashan Mehra <jashanmehra007@gmail.com>
2026-03-29 13:01:24 +0530

    OK

--- chapter 10/03_self.py
+++ chapter 10/03_self.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Employee:
     language = "py"
     salary = 150000
@@ -11,18 +10,4 @@ class Employee:
         print("Good morning")
 harry= Employee()
 # harry = "java script"
-=======
-class Employee:
-    language = "py"
-    salary = 150000
-    
-    def getinfo(self):
-        print(f"The language is {self.language}.The salary is {self.salary}")
-
-    @staticmethod
-    def greet():
-        print("Good morning")
-harry= Employee()
-# harry = "java script"
->>>>>>> 06e56d1c0f03ad01d4fd6bb656a244e13c3084c1
 harry.getinfo()
